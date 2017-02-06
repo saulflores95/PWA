5cea0f37cccb5d1a216d268b4cc70484f41e1d32
Saul Flores <saulpolo95@gmail.com>
2017-02-06 12:20:44 -0800

    Up to a good start
    Webpack and linting setup

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const { resolve } = require("path");
+
+const srcDir = resolve(__dirname, "src");
+
+module.exports = {
+  entry: `${srcDir}/index.js`,
+  output: {
+    filename: 'bundle.js'
+  },
+  module:{
+    rules:[{
+      enforce:'pre',
+      test:/\.js$/,
+      loader:"standard-loader",
+      exclude: /node_modules/
+    }]
+  }
+}
